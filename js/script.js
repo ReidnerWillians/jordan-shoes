4ad964fcf0fdbb877441329aabc34d38a844ca89
Reidner Willians Pacheco Peres <pacheco195920@gmail.com>
2024-02-03 22:16:25 -0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
+const formatCurrency = (number) => {
+    return number.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+}
 const getProducts = async () => {
     const response = await fetch('js/products.json')
     const data = await response.json()
-    
     return data
 }
 
 const generateCard = async () => {
     const products = await getProducts()
-    
-
     products.map(product => {
         
         let card = document.createElement('div')
@@ -21,7 +24,7 @@ const generateCard = async () => {
             <h4>${product.product_name}</h4>
             <h5>${product.product_model}</h5>
         </div>
-        <h6>R$: ${product.price}</h6> `
+        <h6> ${formatCurrency(product.price)}</h6>`
 
         const listaProdutos = document.querySelector('.lista__produtos')
         listaProdutos.appendChild(card)
