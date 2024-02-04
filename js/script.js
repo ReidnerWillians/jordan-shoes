50c6a74facbaeca935ce0a20e1fdb413b9ce85bc
Reidner Willians Pacheco Peres <pacheco195920@gmail.com>
2024-02-03 21:36:10 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const getProducts = async () => {
+    const response = await fetch('js/products.json')
+    const data = await response.json()
+    
+    return data
+}
+
+const generateCard = async () => {
+    const products = await getProducts()
+    
+
+    products.map(product => {
+        
+        let card = document.createElement('div')
+        card.classList.add('card__produto')
+        card.innerHTML = `
+        <figure>
+            <img src="Images/${product.image}" alt="${product.product_name}">
+        </figure>
+        <div class="card__produtos_detalhes">
+            <h4>${product.product_name}</h4>
+            <h5>${product.product_model}</h5>
+        </div>
+        <h6>R$: ${product.price}</h6> `
+
+        const listaProdutos = document.querySelector('.lista__produtos')
+        listaProdutos.appendChild(card)
+})
+}
+generateCard()
